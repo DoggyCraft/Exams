a1f09c3e77b678ec80cbfba6a213145c4bd34d91
fido2603 <fido2603gamer@gmail.com>
2018-11-16 16:54:16 +0100

    Small change to logging, changed from believerName to playerName, might fuck something up, dunno

--- src/com/dogonfire/exams/Commands.java
+++ src/com/dogonfire/exams/Commands.java
@@ -55,7 +55,6 @@ public class Commands
 			if (args.length == 0)
 			{
 				CommandHelp(sender);
-				plugin.log(sender.getName() + ": /exams");
 				return true;
 			}
 			if (args.length == 1)
@@ -69,7 +68,6 @@ public class Commands
 
 					this.plugin.reloadSettings();
 					sender.sendMessage(ChatColor.YELLOW + this.plugin.getDescription().getFullName() + ":" + ChatColor.AQUA + " Reloaded configuration.");
-					this.plugin.log(sender.getName() + ": /exams reload");
 					return true;
 				}
 				if (args[0].equalsIgnoreCase("help"))
@@ -80,7 +78,6 @@ public class Commands
 					}
 
 					CommandList(sender);
-					this.plugin.log(sender.getName() + ": /exams help");
 					return true;
 				}
 				if (args[0].equalsIgnoreCase("clean"))
@@ -91,8 +88,6 @@ public class Commands
 					}
 
 					CommandClean(sender);
-
-					this.plugin.log(sender.getName() + ": /exams clean");
 					return true;
 				}
 				if ((args[0].equalsIgnoreCase("a")) || (args[0].equalsIgnoreCase("b")) || (args[0].equalsIgnoreCase("c")) || (args[0].equalsIgnoreCase("d")))
@@ -108,7 +103,6 @@ public class Commands
 							return false;
 						}
 
-						this.plugin.log(sender.getName() + ": /exams list");
 						return true;
 					}
 
@@ -129,7 +123,6 @@ public class Commands
 						}
 
 						CommandInfo(sender, args[1]);
-						this.plugin.log(sender.getName() + ": /exams info " + args[1]);
 						return true;
 					}
 					if (args[0].equalsIgnoreCase("reset"))
@@ -140,7 +133,6 @@ public class Commands
 						}
 
 						CommandReset(sender, args[1]);
-						this.plugin.log(sender.getName() + ": /exams reset " + args[1]);
 						return true;
 					}
 
