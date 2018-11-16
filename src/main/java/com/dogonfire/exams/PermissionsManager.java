debc31ac7482215d6f6097a27a25d73297e7b6f7
DogOnFire <dogonfire@hotmail.com>
2018-11-16 18:57:57 +0100

    Small fixes

--- src/main/java/com/dogonfire/exams/PermissionsManager.java
+++ src/main/java/com/dogonfire/exams/PermissionsManager.java
@@ -1,25 +1,10 @@
 package main.java.com.dogonfire.exams;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-import com.platymuus.bukkit.permissions.Group;
-import com.platymuus.bukkit.permissions.PermissionsPlugin;
-
-import de.bananaco.bpermissions.api.ApiLayer;
-import de.bananaco.bpermissions.api.WorldManager;
-import de.bananaco.bpermissions.api.util.Calculable;
-import de.bananaco.bpermissions.api.util.CalculableType;
 import net.milkbowl.vault.permission.Permission;
-import ru.tehkode.permissions.PermissionGroup;
-import ru.tehkode.permissions.PermissionManager;
-import ru.tehkode.permissions.PermissionUser;
-import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 /*
 import org.anjocaido.groupmanager.GroupManager;
@@ -32,30 +17,19 @@ public class PermissionsManager
 {
 	private String				pluginName			= "null";
 	private Exams				plugin;
-	private PermissionManager	pex					= null;
 	private Permission 			vaultPermission;
-	private PluginManager 		pluginManager;
 	
 	public PermissionsManager(Exams p)
 	{
 		this.plugin = p;
-		
-		pluginManager = plugin.getServer().getPluginManager();
-		
+			
 		RegisteredServiceProvider<Permission> permissionProvider = plugin.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
 		vaultPermission = permissionProvider.getProvider();
 	}
 
 	public void load()
 	{
-		pluginManager = plugin.getServer().getPluginManager();
-
-		if (pluginManager.getPlugin("PermissionsEx") != null)
-		{
-			plugin.log("Using PermissionsEx.");
-			pluginName = "PermissionsEx";
-			pex = PermissionsEx.getPermissionManager();
-		}
+		// Nothing to see here
 	}
 
 	public Plugin getPlugin()
